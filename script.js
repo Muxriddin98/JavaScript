329369d80f65f60a6d8769b21c291c03d3bf149b
muxriddin <mukhir98@gmail.com>
2022-07-11 19:12:01 +0500

    javascript

---
+++ script.js
@@ -0,0 +1,42 @@
+
+var name =  prompt('Введите своё имя');
+
+console.log('Ваш имя ' + name);
+
+
+var age = +prompt('Введите свое возраст');
+
+console.log('Ваш возраст ' + age);
+
+
+var addition = +prompt('Напишите правильный ответ 60 + 3 = ?');
+console.log('Ваш ответ ' + addition);
+alert('Правильный ответ 63');
+
+var subtraction = +prompt('Напишите правильный ответ 60 - 3 = ?');
+console.log('Ваш ответ ' + substraction);
+alert('Правильный ответ 57');
+
+var multiplication = +prompt('Напишите правильный ответ 60 * 3 = ?');
+console.log('Ваш ответ ' + multiplication);
+alert('Правильный ответ 180');
+
+var separation = +prompt('Напишите правильный ответ 60 / 3 = ?');
+console.log('Ваш ответ ' + separation);
+alert('Правильный ответ 20');
+
+var percent = +prompt('Напишите правильный ответ 100 % 30  = ?');
+console.log('Ваш ответ ' + percent);
+alert('Правильный ответ 10');
+
+
+alert('Откройте консоль для проверки все данные');
+
+
+
+
+
+
+
+
+
